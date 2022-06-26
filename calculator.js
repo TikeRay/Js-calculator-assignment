d8fef30c3de39204ebe4367b557752b350aec464
TikeRay <105748361+TikeRay@users.noreply.github.com>
2022-06-26 13:56:29 +0100

    calculator

---
+++ calculator.js
@@ -0,0 +1,15 @@
+let opr = prompt ("Operator:  ")
+let num1 = parseInt(prompt("Number 1:  "))
+let num2 = parseInt(prompt("Number 2  "))
+
+let result;
+if (opr == "+") {
+  result = num1 + num2;
+} else if (opr == "-") {
+    result = num1 - num2;
+} else if (opr == "*") {
+    result = num1 * num2;
+} else if (opr == "/") {
+    result = num1 / num2;
+}
+alert ("Answer:" + " " + result);
